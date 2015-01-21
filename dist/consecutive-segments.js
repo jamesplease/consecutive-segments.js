e6406c57f3a81b6d3b6d6006e708e7895d889fde
Jmeas <jellyes2@gmail.com>
2015-01-21 09:46:52 -0500

    v0.2.0

--- dist/consecutive-segments.js
+++ dist/consecutive-segments.js
@@ -6,57 +6,48 @@
     var moment = require("moment");
     module.exports = factory(_, moment);
   } else {
-    root.ConsecutiveSegments = factory(root._, root.moment);
+    root.consecutiveSegments = factory(root._, root.moment);
   }
 })(this, function (_, moment) {
   "use strict";
 
-  var ConsecutiveSegments = {
+  // Segment an array of events by scale
+  var consecutiveSegments = function (segments) {
+    var scale = arguments[1] === undefined ? "weeks" : arguments[1];
+    if (_.isEmpty(segments)) {
+      return [];
+    }
 
-    // Segment an array of events by scale
-    group: function group(segments) {
-      var scale = arguments[1] === undefined ? "weeks" : arguments[1];
-      var segmentCount = _.size(segments);
-
-      if (!segments || segmentCount === 0) {
-        return [];
+    var currentGroup = 0,
+        currentMoment = undefined,
+        prevMoment = undefined;
+    return _.chain(segments).map(function (events, timestamp) {
+      return { timestamp: timestamp, events: events };
+    }).reduce(function (memo, s, index, segments) {
+      // Check to see if the current group is the same
+      // as the previous group by computing the difference
+      // in their timestamps. They aren't consecutive when
+      // the difference is > 1
+      if (index) {
+        currentMoment = moment.unix(s.timestamp).utc();
+        prevMoment = moment.unix(segments[index - 1].timestamp).utc();
+        if (currentMoment.diff(prevMoment, scale) > 1) {
+          currentGroup++;
+        }
       }
 
-      var currentGroup = 0, groups = [], currentMoment, prevMoment;
-      _.chain(segments).map(function (events, timestamp) {
-        return { timestamp: timestamp, events: events };
-      }).each(function (s, index, segments) {
-        // Check to see if the current group is the same
-        // as the previous group by computing the difference
-        // in their timestamps. > 1 means that they aren't
-        // consecutive.
-        if (index !== 0) {
-          currentMoment = moment.unix(s.timestamp).utc();
-          prevMoment = moment.unix(segments[index - 1].timestamp).utc();
-          if (currentMoment.diff(prevMoment, scale) > 1) {
-            currentGroup++;
-          }
-        }
-
-        // Ensure that the group exists, then push to it
-        if (!groups[currentGroup]) {
-          groups[currentGroup] = [];
-        }
-        groups[currentGroup].push(_.clone(s));
-      });
-
-      return groups;
-    }
+      // Ensure that the group exists, then push to it
+      if (!memo[currentGroup]) {
+        memo[currentGroup] = [];
+      }
+      memo[currentGroup].push(_.clone(s));
+      return memo;
+    }, []).value();
   };
 
 
 
 
-  return ConsecutiveSegments;
+  return consecutiveSegments;
 });
-// Place your library's code here
-//
-// If you add additional files, be sure to
-// load them in order in ./wrapper.js
-//
 //# sourceMappingURL=consecutive-segments.js.map
